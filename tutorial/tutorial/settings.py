03bb5df074f0810d0a0654cf5ef7333533e696ac
Daniel Rus Morales <danirus@eml.cc>
2018-02-13 18:46:49 +0100

    JavaScript plugin

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'django_markdown2',
     'django_comments_xtd',
     'django_comments',
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
+from django.views.i18n import JavaScriptCatalog
 
 
 urlpatterns = [
@@ -11,6 +12,7 @@ urlpatterns = [
     path(r'comments/', include('django_comments_xtd.urls')),
     path(r'', TemplateView.as_view(template_name="home.html"),
         name='homepage'),
+    path(r'jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 ]
 
 
